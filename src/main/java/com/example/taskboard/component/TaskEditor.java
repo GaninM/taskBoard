333640b5eb933e6c21b15875f6b516b66556716e
Maksim Ganin <maksim_ganin@epam.com>
2022-05-23 14:18:25 +0400

    add comboBox

--- src/main/java/com/example/taskboard/component/TaskEditor.java
+++ src/main/java/com/example/taskboard/component/TaskEditor.java
@@ -5,6 +5,7 @@ import com.example.taskboard.model.Task;
 import com.example.taskboard.repository.TaskRepository;
 import com.vaadin.flow.component.KeyNotifier;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -16,6 +17,9 @@ import com.vaadin.flow.spring.annotation.UIScope;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringComponent
 @UIScope
 public class TaskEditor extends VerticalLayout implements KeyNotifier {
@@ -25,7 +29,8 @@ public class TaskEditor extends VerticalLayout implements KeyNotifier {
     private Task task;
 
     /* Field to edit properties in Task entity */
-    private final TextField type = new TextField("Task type");
+    //private final TextField type = new TextField("Task type");
+    private final ComboBox<String> comboBox = new ComboBox<>("Task type");
     private final TextField inputData = new TextField("Input Data");
 
     /* Action buttons*/
@@ -43,10 +48,17 @@ public class TaskEditor extends VerticalLayout implements KeyNotifier {
         this.taskRepository = taskRepository;
         this.binder = new Binder<>(Task.class);
 
-        inputData.setWidth("1000");
-        add(type, inputData, save, cancel, delete);
-        type.setWidth("1000");
+        List<String> taskTypes = new ArrayList<>();
+        taskTypes.add("Substrings");
+        taskTypes.add("Magic square");
+        comboBox.setAllowCustomValue(true);
+        comboBox.setItems(taskTypes);
+        comboBox.setValue("Substrings");
+        //comboBox.addValueChangeListener(e -> showField(e.getValue()));
 
+        inputData.setWidth("1000");
+
+        add(comboBox, inputData, save, cancel, delete);
 
         binder.bindInstanceFields(this);
 
